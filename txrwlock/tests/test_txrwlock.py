8da4765595b10ff87006824c6a58158cf8b94a98
Gaetan Semet <gaetan@xeberon.net>
2016-09-27 18:22:23 +0200

    first version (unit test fails)

--- txrwlock/tests/test_txrwlock.py
+++ txrwlock/tests/test_txrwlock.py
@@ -1,27 +1,83 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import logging
 
-"""
-test_txrwlock
-----------------------------------
-Tests for `txrwlock` module.
-"""
+from twisted.internet import base
+from twisted.internet import defer
+from twisted.internet import reactor
+from twisted.internet import task
+from twisted.trial.unittest import TestCase
 
-import pytest
-
-from txrwlock import txrwlock
+from txrwlock.deferred_readers_writer_lock import DeferredReadersWriterLock
 
 
-class TestTxrwlock(object):
+logger = logging.getLogger(__name__)
 
-    @classmethod
-    def setup_class(cls):
-        pass
 
-    @classmethod
-    def teardown_class(cls):
-        pass
+def sleep(numSec):
+    return task.deferLater(reactor, numSec, lambda: None)
 
-    def test_something(self):
-        pass
 
+class ReadersWriterDeferredLockTestCase(TestCase):
+
+    @defer.inlineCallbacks
+    def testSimpleReads(self):
+        lock = DeferredReadersWriterLock()
+        yield lock.readerAcquire()
+        yield lock.readerAcquire()
+        self.assertFalse(lock.isWriting)
+        yield lock.readerRelease()
+        yield lock.readerRelease()
+
+    @defer.inlineCallbacks
+    def testSimpleWrites(self):
+        lock = DeferredReadersWriterLock()
+        self.assertFalse(lock.isWriting)
+        yield lock.writerAcquire()
+        self.assertTrue(lock.isWriting)
+        yield lock.writerRelease()
+        self.assertFalse(lock.isWriting)
+
+    # @TxTestCase.verboseLogging()
+    @defer.inlineCallbacks
+    def testWrites(self):
+        # Set debug to True to get full traceback of unclean deferred
+        self.patch(base.DelayedCall, "debug", False)
+        lock = DeferredReadersWriterLock()
+        self.shared_var = 10
+
+        @defer.inlineCallbacks
+        def read(delay, duration, expectedVals):
+            if delay:
+                yield sleep(delay)
+            yield lock.readerAcquire()
+            if duration:
+                yield sleep(duration)
+            logger.debug("Reading shared_var=%r", self.shared_var)
+            if isinstance(expectedVals, list):
+                self.assertIn(self.shared_var, expectedVals)
+            else:
+                self.assertEqual(expectedVals, self.shared_var)
+            yield lock.readerRelease()
+
+        @defer.inlineCallbacks
+        def write(delay, duration, newVal):
+            if delay:
+                yield sleep(delay)
+            yield lock.writerAcquire()
+            if duration:
+                yield sleep(duration)
+            logger.debug("Writing shared_var=%r", self.shared_var)
+            self.shared_var = newVal
+            yield lock.writerRelease()
+
+        yield defer.gatherResults([
+            read(0.1, 0.1, 10),
+            read(0.1, 0.1, 10),
+            write(0.15, 0.1, 15),
+            write(0.17, 0.1, 20),  # should block until end of previous write
+            read(0.2, 0, [15, 20]),  # '15' if read unlocked before write, else 20
+            read(0.26, 0, 20),  # Should be blocked and return result of the second write
+            read(0.30, 0, 20),  # Should not be blocked
+        ])
+        logger.debug("=== TEST FINISHED ===")
--- txrwlock/txrwlock.py
+++ txrwlock/txrwlock.py
@@ -1,1 +1,134 @@
 # -*- coding: utf-8 -*-
+from twisted.internet import defer
+
+
+class __LightSwitch(object):
+
+    '''
+    An auxiliary "light switch"-like object. The first deferred turns on the "switch", the
+    last one turns it off.
+    '''
+
+    def __init__(self):
+        self.__cnt = 0
+        self.__m = defer.DeferredLock()
+
+    @defer.inlineCallbacks
+    def acquire(self, lock):
+        yield self.__m.acquire()
+        self.__cnt += 1
+        if self.__cnt == 1:
+            yield lock.acquire()
+        self.__m.release()
+
+    @defer.inlineCallbacks
+    def release(self, lock):
+        yield self.__m.acquire()
+        self.__cnt -= 1
+        if self.__cnt == 0:
+            lock.release()
+        self.__m.release()
+
+
+class ReadersWriterDeferredLock(object):
+
+    # Source:
+    #   http://code.activestate.com/recipes/577803-reader-writer-lock-with-priority-for-writers/
+    # License:
+    #   MIT License
+
+    '''
+    Readers-Writer Lock for Twisted's Deferred
+
+    Many readers can simultaneously access a share at the same time, but a writer has an exclusive
+    access to this share.
+
+    The following constraints should be met:
+    1) no reader should be kept waiting if the share is currently not opened to anyone or to only
+       other readers.
+    2) only one writer can open the share at the same time, and when multiple writer request access,
+       they will waiting for the execution of all previous writer access
+
+    Reads and Writes are executed from within the main twisted reactor. Do **NOT** call it from
+    external threads (e.g., from synchronous method execute in thread with ``deferToThread``).
+
+    Description
+    -----------
+
+    "Readers" uses ``readerAcquire`` and ``readRelease``.
+    "Writer" uses ``writerAcquire`` and ``writerRelease``.
+
+    Python version > 3.5 can also use `async with reader`
+
+    A "reader" is not blocked when 2 or more 'reads' are executing.
+    A "reader" is blocked when a 'write' is executing.
+
+    When a "write" is started, it blocks all new 'reads' and wait for the pending 'reads' to finish.
+    If a new 'write" is requested, it will wait for running writes to finish as well.
+
+    Notes:
+
+        Please be aware than ``DeferredReadersWriterLock.acquire*`` and
+        ``DeferredReadersWriterLock.release*`` methods are deferred, which is different from
+        ``defer.DeferredLock``, where only the ``defer.DeferredLock.acquire()`` method is a
+        deferred.
+
+    Usage
+    -----
+
+    Threads that just need "read" access, use the following pattern:
+
+    .. code-block:: yaml
+
+        @defer.inlineCallbacks
+        def aReaderMethod(...):
+            try:
+                yield aServer.readerAcquire()
+                ... any treatment ...
+            finally:
+                yield aServer.readRelease()
+
+        @defer.inlineCallbacks
+        def aWriterMethod(...):
+            try:
+                yield aServer.writerAcquire()
+                ... any treatment ...
+            finally:
+                yield aServer.writerRelease()
+    '''
+
+    def __init__(self):
+        self.__rd_swtch = __LightSwitch()
+        self.__wrte_swtch = __LightSwitch()
+        self.__no_rdr = defer.DeferredLock()
+        self.__no_wrtr = defer.DeferredLock()
+        self.__rdrs_q = defer.DeferredLock()
+
+    @property
+    def isWriting(self):
+        return self.__no_rdr.locked
+
+    @defer.inlineCallbacks
+    def readerAcquire(self):
+        yield self.__rdrs_q.acquire()
+        yield self.__no_rdr.acquire()
+        yield self.__rd_swtch.acquire(self.__no_wrtr)
+        self.__no_rdr.release()
+        self.__rdrs_q.release()
+
+    @defer.inlineCallbacks
+    def readRelease(self):
+        yield self.__rd_swtch.release(self.__no_wrtr)
+
+    @defer.inlineCallbacks
+    def writerAcquire(self):
+        yield self.__wrte_swtch.acquire(self.__no_rdr)
+        yield self.__no_wrtr.acquire()
+
+    @defer.inlineCallbacks
+    def writerRelease(self):
+        self.__no_wrtr.release()
+        yield self.__wrte_swtch.release(self.__no_rdr)
+
+
+__all__ = [DeferredReadersWriterLock]
